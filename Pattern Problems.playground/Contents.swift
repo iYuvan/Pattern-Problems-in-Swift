4b701fe8f839cfae6811ce8ac26d4ec8d1c13899
iYuvan <76487351+iYuvan@users.noreply.github.com>
2022-09-05 23:33:56 +0530

    Add files via upload

---
+++ Pattern Problems.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+// Square Star Pattern
+func squareStar(input: Int) {
+    for _ in 1...5 {
+        for _ in 1..<5 {
+            print("*", terminator: "")
+        }
+        print("*")
+    }
+}
+//squareStar(input: 5)
+
+
+// Right Triangle Star Pattern
+func rightTriangle()  {
+    for i in 1...5{
+        for _ in 1...i{
+          print("*", terminator: "")
+        }
+        print("")
+    }
+}
+//rightTriangle()
